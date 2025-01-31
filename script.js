5a6740a220c68c6287f51ddff5cdd31376c3df36
white674 <136436389+white674@users.noreply.github.com>
2025-01-31 09:19:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
